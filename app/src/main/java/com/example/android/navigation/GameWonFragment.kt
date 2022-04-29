2dc00235009bd4d185274d4a8e21a3a00ada1662
Rylder Oliveira <rylder2014@gmail.com>
2022-04-29 09:52:12 -0300

    Add args in won activity

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -16,10 +16,12 @@
 
 package com.example.android.navigation
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -32,7 +34,19 @@ class GameWonFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_game_won, container, false)
-       binding.nextMatchButton.setOnClickListener { view: View -> view.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment) }
+        binding.nextMatchButton.setOnClickListener { view: View -> view.findNavController().navigate(GameWonFragmentDirections.actionGameWonFragmentToGameFragment()) }
+        val args = GameWonFragmentArgs.fromBundle(arguments!!)
+        Toast.makeText(context, "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}", Toast.LENGTH_LONG).show()
+        setHasOptionsMenu(true)
         return binding.root
     }
+
+    fun getShareIntent(): Intent {
+        val args = GameWonFragmentArgs.fromBundle(arguments)
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.setType("text/plain").putExtra(Intent.EXTRA_TEXT, getString(R.string.share_success_text, args.numCorrect, args.numQuestions))
+        return shareIntent
+    }
+
+
 }
